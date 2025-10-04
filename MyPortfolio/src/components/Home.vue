9993e428df12784fea81482eee3d04f301cfc3df
BOBEIRAS <darkboblapa@gmail.com>
2025-10-04 21:50:48 +0100

    changed home

--- MyPortfolio/src/components/Home.vue
+++ MyPortfolio/src/components/Home.vue
@@ -2,7 +2,7 @@
   <section class="home">
     <h1>Welcome to my portfolio</h1>
     <p>Hi, Im BOBEIRAS. Im a Student in Management and Programming of Information Systems passionate about programing,listen to music and to learn more !.</p>
-    <img src="../icons/pfp.jpeg" alt="Profile Picture" class="profile-pic">
+    <img src="@/components/icons/pfp.jpeg" alt="Profile Picture" class="profile-pic">
   </section>
 </template>
 
@@ -16,5 +16,13 @@
   height: 200px;
   border-radius: 50%;
   margin-top: 1rem;
+}
+h1{
+  color:#ff0055;
+  font-size: 2.5rem;
+  text-align: center;
+  margin-bottom: 1.5rem;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
 }
 </style>
